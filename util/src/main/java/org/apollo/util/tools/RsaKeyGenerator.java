877bab7eb8937f5c5b9d9fc779543175763f72ee
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2023-01-31 03:08:00 -0500

    Removed bouncy castle dep

--- util/src/main/java/org/apollo/util/tools/RsaKeyGenerator.java
+++ util/src/main/java/org/apollo/util/tools/RsaKeyGenerator.java
@@ -1,15 +1,9 @@
 package org.apollo.util.tools;
 
-import java.io.FileWriter;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.Security;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemWriter;
 
 /**
  * An RSA key generator.
@@ -17,6 +11,7 @@ import org.bouncycastle.util.io.pem.PemWriter;
  * @author Graham
  * @author Major
  * @author Cube
+ * @author Advocatus
  */
 public final class RsaKeyGenerator {
 
@@ -27,19 +22,12 @@ public final class RsaKeyGenerator {
 	private static final int BIT_COUNT = 1024;
 
 	/**
-	 * The path to the private key file.
-	 */
-	private static final String PRIVATE_KEY_FILE = "data/rsa.pem";
-
-	/**
 	 * The entry point of the RsaKeyGenerator.
 	 *
 	 * @param args The application arguments.
 	 */
 	public static void main(String[] args) throws Exception {
-		Security.addProvider(new BouncyCastleProvider());
-
-		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 		keyPairGenerator.initialize(BIT_COUNT);
 		KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
@@ -51,12 +39,10 @@ public final class RsaKeyGenerator {
 		System.out.println("public key: " + publicKey.getPublicExponent());
 		System.out.println("modulus: " + publicKey.getModulus());
 
-		try (PemWriter writer = new PemWriter(new FileWriter(PRIVATE_KEY_FILE))) {
-			writer.writeObject(new PemObject("RSA PRIVATE KEY", privateKey.getEncoded()));
-		} catch (Exception e) {
-			System.err.println("Failed to write private key to " + PRIVATE_KEY_FILE);
-			e.printStackTrace();
-		}
+		System.out.println("Place these keys in the server:");
+		System.out.println("--------------------");
+		System.out.println("private key: " + privateKey.getPrivateExponent());
+		System.out.println("modulus: " + privateKey.getModulus());
 	}
 
 }
